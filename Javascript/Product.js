8d5c0fb3d74ecaf191a95215b7a9cfe85805b8c0
Mohammed Al-hameen <mohammedalhameen@gmail.com>
2023-08-21 16:27:27 +0100

    SignUp page done

--- Javascript/Product.js
+++ Javascript/Product.js
@@ -10,28 +10,28 @@ const Lunch = [
           {
             id: 1,
             name: 'Fried Rice',
-            image: '../img/friedRice.jpg',
+            image: './img/friedRice.jpg',
             price: 300,
             unitMassPortion: 180+'g'
           },
           {
             id: 2,
             name: 'Jollof Rice',
-            image: '../img/jollofRice.jpg',
+            image: './img/jollofRice.jpg',
             price: 300,
             unitMassPortion: 180+'g'
           },
           {
             id: 3,
             name: 'Rice and Beans',
-            image: '../img/riceBeans.jpg',
+            image: './img/riceBeans.jpg',
             price: 300,
             unitMassPortion: 180+'g'
           },
           {
             id: 4,
             name: 'Jollof Spaghetti',
-            image: '../img/jollofSpaghetti.jpg',
+            image: './img/jollofSpaghetti.jpg',
             price: 300,
             unitMassPortion: 180+'g'
           },
@@ -45,28 +45,28 @@ const Lunch = [
           {
             id: 1,
             name: 'Fried Plantain',
-            image: '../img/friedPlantain.jpg',
+            image: './img/friedPlantain.jpg',
             price: 150,
             unitMassPortion: 100+'g'
           },
           {
             id: 2,
             name: 'Yam Pottage',
-            image: '../img/yamPottage.jpg',
+            image: './img/yamPottage.jpg',
             price: 300,
             unitMassPortion: 180+'g'
           },
           {
             id: 3,
             name: 'Sweet Potato',
-            image: '../img/sweetPotato.jpg',
+            image: './img/sweetPotato.jpg',
             price: 250,
             unitMassPortion: 180+'g'
           },
           {
             id: 4,
             name: 'Savoury Beans',
-            image: '../img/savouryBeans.jpg',
+            image: './img/savouryBeans.jpg',
             price: 300,
             unitMassPortion: 180+'g'
           },
@@ -81,35 +81,35 @@ const Lunch = [
           {
             id: 1,
             name: 'Eba',
-            image: '../img/eba.jpg',
+            image: './img/eba.jpg',
             price: 100,
             unitMassPortion: 150+'g'
           },
           {
             id: 2,
             name: 'Semo',
-            image: '../img/semo.jpg',
+            image: './img/semo.jpg',
             price: 100,
             unitMassPortion: 150+'g'
           },
           {
             id: 3,
             name: 'Egusi Soup',
-            image: '../img/egusi.jpg',
+            image: './img/egusi.jpg',
             price: 350,
             unitMassPortion: 100+'g'
           },
           {
             id: 4,
             name: 'Okro Soup',
-            image: '../img/okro.jpg',
+            image: './img/okro.jpg',
             price: 350,
             unitMassPortion: 100+'g'
           },
           {
             id: 5,
             name: 'Oha Soup',
-            image: '../img/oha.jpg',
+            image: './img/oha.jpg',
             price: 350,
             unitMassPortion: 100+'g'
           },
@@ -117,7 +117,7 @@ const Lunch = [
           {
             id: 6,
             name: 'Nsala Soup',
-            image: '../img/nsala.jpg',
+            image: './img/nsala.jpg',
             price: 350,
             unitMassPortion: 100+'g'
           },
@@ -132,42 +132,42 @@ const Lunch = [
           {
             id: 1,
             name: 'Fried Beef',
-            image: '../img/friedBeef.jpg',
+            image: './img/friedBeef.jpg',
             price: 250,
             unitMassPortion: 50+'g'
           },
           {
             id: 2,
             name: 'fried Fish',
-            image: '../img/friedFish.jpg',
+            image: './img/friedFish.jpg',
             price: 250,
             unitMassPortion: 50+'g'
           },
           {
             id: 3,
             name: 'Hotdog',
-            image: '../img/hotdog.jpg',
+            image: './img/hotdog.jpg',
             price: 150,
             unitMassPortion: 50+'g'
           },
           {
             id: 4,
             name: 'Chicken Casserole',
-            image: '../img/chickenCasserole.jpg',
+            image: './img/chickenCasserole.jpg',
             price: 900,
             unitMassPortion: 50+'g'
           },
           {
             id: 5,
             name: 'Chicken Wing',
-            image: '../img/chickenWings.jpg',
+            image: './img/chickenWings.jpg',
             price: 350,
             unitMassPortion: 50+'g'
           },
           {
             id: 6,
             name: 'Boiled Egg',
-            image: '../img/boiledEgg.jpg',
+            image: './img/boiledEgg.jpg',
             price: 150,
             unitMassPortion: 50+'g'
           },
@@ -182,37 +182,37 @@ const Lunch = [
           {
             id: 1,
             name: 'Bottled Water',
-            image: '../img/bottledWater.jpg',
+            image: './img/bottledWater.jpg',
             price: 100,
           },
           {
             id: 2,
             name: 'Freshyo',
-            image: '../img/freshyo.jpg',
+            image: './img/freshyo.jpg',
             price: 400,
           },
           {
             id: 3,
             name: 'Viju Choco',
-            image: '../img/vijuChoco.jpg',
+            image: './img/vijuChoco.jpg',
             price: 400,
           },
           {
             id: 4,
             name: 'Can Malt',
-            image: '../img/canMalt.jpg',
+            image: './img/canMalt.jpg',
             price: 300,
           },
           {
             id: 5,
             name: 'Pineapple Juice',
-            image: '../img/pineapple.jpg',
+            image: './img/pineapple.jpg',
             price: 300,
           },
           {
             id: 6,
             name: 'Zobo',
-            image: '../img/zobo.jpg',
+            image: './img/zobo.jpg',
             price: 300,
           },
           
@@ -230,35 +230,35 @@ const Lunch = [
           {
             id: 1,
             name: 'Meatpie',
-            image: '../img/meatpie.jpg',
+            image: './img/meatpie.jpg',
             price: 250,
             unitMassPortion: 50+'g'
           },
           {
             id: 2,
             name: 'Chicken pie',
-            image: '../img/chickenpie.jpg',
+            image: './img/chickenpie.jpg',
             price: 250,
             unitMassPortion: 50+'g'
           },
           {
             id: 3,
             name: 'Sausage',
-            image: '../img/sausage.jpg',
+            image: './img/sausage.jpg',
             price: 150,
             unitMassPortion: 50+'g'
           },
           {
             id: 4,
             name: 'Eggroll',
-            image: '../img/eggroll.jpg',
+            image: './img/eggroll.jpg',
             price: 900,
             unitMassPortion: 50+'g'
           },
           {
             id: 5,
             name: 'Doughnut',
-            image: '../img/doughnut.jpg',
+            image: './img/doughnut.jpg',
             price: 350,
             unitMassPortion: 50+'g'
           },
@@ -272,31 +272,31 @@ const Lunch = [
           {
             id: 1,
             name: 'Bottled Water',
-            image: '../img/bottledWater.jpg',
+            image: './img/bottledWater.jpg',
             price: 100,
           },
           {
             id: 2,
             name: 'Freshyo',
-            image: '../img/freshyo.jpg',
+            image: './img/freshyo.jpg',
             price: 400,
           },
           {
             id: 3,
             name: 'Viju Choco',
-            image: '../img/vijuChoco.jpg',
+            image: './img/vijuChoco.jpg',
             price: 400,
           },
           {
             id: 4,
             name: 'Zobo',
-            image: '../img/zobo.jpg',
+            image: './img/zobo.jpg',
             price: 300,
           },
           {
             id: 5,
             name: 'Chi Exotic',
-            image: '../img/chiExotic.jpg',
+            image: './img/chiExotic.jpg',
             price: 300,
           },
     
@@ -315,35 +315,35 @@ const Lunch = [
           {
             id: 1,
             name: 'Meatpie',
-            image: '../img/meatpie.jpg',
+            image: './img/meatpie.jpg',
             price: 250,
             unitMassPortion: 50+'g'
           },
           {
             id: 2,
             name: 'Chicken pie',
-            image: '../img/chickenpie.jpg',
+            image: './img/chickenpie.jpg',
             price: 250,
             unitMassPortion: 50+'g'
           },
           {
             id: 3,
             name: 'Sausage',
-            image: '../img/sausage.jpg',
+            image: './img/sausage.jpg',
             price: 150,
             unitMassPortion: 50+'g'
           },
           {
             id: 4,
             name: 'Eggroll',
-            image: '../img/eggroll.jpg',
+            image: './img/eggroll.jpg',
             price: 900,
             unitMassPortion: 50+'g'
           },
           {
             id: 5,
             name: 'Doughnut',
-            image: '../img/doughnut.jpg',
+            image: './img/doughnut.jpg',
             price: 350,
             unitMassPortion: 50+'g'
           },
@@ -357,31 +357,31 @@ const Lunch = [
           {
             id: 1,
             name: 'Bottled Water',
-            image: '../img/bottledWater.jpg',
+            image: './img/bottledWater.jpg',
             price: 100,
           },
           {
             id: 2,
             name: 'Freshyo',
-            image: '../img/freshyo.jpg',
+            image: './img/freshyo.jpg',
             price: 400,
           },
           {
             id: 3,
             name: 'Viju Choco',
-            image: '../img/vijuChoco.jpg',
+            image: './img/vijuChoco.jpg',
             price: 400,
           },
           {
             id: 4,
             name: 'Zobo',
-            image: '../img/zobo.jpg',
+            image: './img/zobo.jpg',
             price: 300,
           },
           {
             id: 5,
             name: 'Pineapple Juice',
-            image: '../img/pineapple.jpg',
+            image: './img/pineapple.jpg',
             price: 300,
           },
         ]
