4b51e08024693b03ce8ab123fc8957ee197c5a8b
Mohammed Al-hameen <mohammedalhameen@gmail.com>
2023-10-04 23:44:53 +0100

    bye

--- Javascript/sendEmail.js
+++ Javascript/sendEmail.js
@@ -17,7 +17,7 @@ async function updateOtpEnabled(enabled) {
 
 // Define constants for time values
 const EMAIL_HOUR = 23;
-const EMAIL_MINUTE = 40;
+const EMAIL_MINUTE = 50;
 const EMAIL_SECOND = 1;
 const DISABLE_OTP_HOUR = 17;
 
